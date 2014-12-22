89b1fc5ea98c816154af33debd6464ecf08d2faf
Jonathan Castello <twisolar@gmail.com>
2014-12-22 02:02:20 -0800

    Refactorings and such.

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,22 @@
-#![feature(slicing_syntax, globs)]
+#![feature(slicing_syntax, globs, unboxed_closures)]
 #![allow(dead_code, unused_parens)]
-use parser::{ParseState, TelnetToken, TelnetTokenizer};
+use parser::{TelnetTokenizer};
+use analyzer::{ChannelData, TelnetDemuxer};
+
 mod parser;
-
-#[deriving(Copy)]
-struct QState {
-  status: [u8, ..256]
-}
-
-impl QState {
-  fn new() -> QState {
-    return QState{status: [0, ..256]}
-  }
-}
-
-#[deriving(Copy)]
-struct TelnetDemuxer {
-  active_channel: Option<u8>,
-  qstate: QState,
-}
-
-#[deriving(Show, Copy)]
-enum TelnetPacket<'a> {
-  Text(&'a [u8]),
-  Command(u8),
-}
-
-#[deriving(Show, Copy)]
-struct ChannelData<'a> {
-  channel: Option<u8>,
-  message: TelnetPacket<'a>,
-}
-
-fn analyze<'a>(state: TelnetDemuxer, token: TelnetToken<'a>) -> (TelnetDemuxer, ChannelData<'a>) {
-  match token {
-    TelnetToken::Text(data) => {
-      (state, ChannelData{channel: state.active_channel, message: TelnetPacket::Text(data)})
-    }
-    TelnetToken::Command(command) => {
-      let newstate = (if command == 0xF0 {
-        TelnetDemuxer {active_channel: None, ..state}
-      } else {
-        state
-      });
-      (newstate, ChannelData{channel: None, message: TelnetPacket::Command(command)})
-    }
-    TelnetToken::Negotiation{command, channel} => {
-      let newstate = (if command == 0xFA {
-        TelnetDemuxer {active_channel: Some(channel), ..state}
-      } else {
-        state
-      });
-      (newstate, ChannelData{channel: Some(channel), message: TelnetPacket::Command(command)})
-    }
-  }
-}
+mod analyzer;
 
 fn main() {
-  let tokenizer = TelnetTokenizer {
-    state: ParseState::Neutral,
-    data: b"\xFF\xFA\x20hello, world!\xFF\xF0"
-  };
+  let stream = [b"\xFF\xFA\x20hello, w\xFF\xFForld!\xFF", b"\xF0"];
 
-  let demuxer = TelnetDemuxer {
-    active_channel: None,
-    qstate: QState::new(),
-  };
+  let mut tokenizer = TelnetTokenizer::new();
+  let mut demuxer = TelnetDemuxer::new();
 
-  let mut iter = tokenizer.map(|token| analyze(demuxer, token));
-  while let Some(packet) = iter.next() {
-    println!("[{}]: {}", foo(&packet.1), &packet.1.message);
+  for &data in stream.iter() {
+    for token in tokenizer.tokenize(data) {
+      let packet = demuxer.analyze(token);
+      println!("[{}]: {}", foo(&packet), &packet.message);
+    }
   }
 }
 
--- src/parser.rs
+++ src/parser.rs
@@ -15,107 +15,129 @@ pub enum TelnetToken<'a> {
   Negotiation{command: u8, channel: u8},
 }
 
-#[deriving(Show)]
-pub struct TelnetTokenizer<'a> {
+// How to assign a custom `is_long_command`:
+//  tokenizer.is_long_command = (box |&: _: u8| false) as Box<Fn(u8) -> bool>;
+pub struct TelnetTokenizer {
   pub state: ParseState,
-  pub data: &'a [u8],
+
+  // TODO: Once types of unboxed closures can be written,
+  //   replace this with a trait Pred: Fn<(u8), bool>.
+  pub is_long_command: Box<Fn<(u8,), bool> + 'static>,
 }
 
-impl<'a> Iterator<TelnetToken<'a>> for TelnetTokenizer<'a> {
-  fn next(&mut self) -> Option<TelnetToken<'a>> {
-    while !self.data.is_empty() {
-      let (token, state, data) = step_parser(self.state, self.data);
-      self.state = state;
-      self.data = data;
+impl TelnetTokenizer {
+  pub fn new() -> TelnetTokenizer {
+    TelnetTokenizer {
+      state: ParseState::Neutral,
+      is_long_command: box |cmd| 250 <= cmd && cmd <= 254,
+    }
+  }
 
+  pub fn tokenize<'a, 'b>(&'a mut self, data: &'b [u8]) -> TokenStream<'a, 'b> {
+    TokenStream {
+      state: self,
+      data: data,
+    }
+  }
+}
+
+
+pub struct TokenStream<'a, 'b> {
+  pub state: &'a mut TelnetTokenizer,
+  pub data: &'b [u8],
+}
+
+type ParseResult<'b> = (Option<TelnetToken<'b>>, ParseState, &'b [u8]);
+
+impl<'a, 'b> TokenStream<'a, 'b> {
+  fn carriage_state(&self) -> ParseResult<'b> {
+    if self.data[0] == b'\n' {
+      (Some(TelnetToken::Text(b"\r\n")), Neutral, self.data[1..])
+    } else if self.data[0] == b'\0' {
+      (Some(TelnetToken::Text(b"\r")), Neutral, self.data[1..])
+    } else {
+      // invalid stream, technically, but still unambiguous
+      (Some(TelnetToken::Text(b"\r")), Neutral, self.data)
+    }
+  }
+
+  fn command_state(&self) -> ParseResult<'b> {
+    if (*self.state.is_long_command).call((self.data[0],)) {
+      (None, Subchannel(self.data[0]), self.data[1..])
+    } else if self.data[0] == b'\xFF' {
+      (Some(TelnetToken::Text(b"\xFF")), Neutral, self.data[1..])
+    } else {
+      (Some(TelnetToken::Command(self.data[0])), Neutral, self.data[1..])
+    }
+  }
+
+  fn subchannel_state(&self, command: u8) -> ParseResult<'b> {
+    (Some(TelnetToken::Negotiation{command: command, channel: self.data[0]}), Neutral, self.data[1..])
+  }
+
+  fn neutral_state(&self) -> ParseResult<'b> {
+    let maybe_idx = self.data.iter().position(|ch: &u8| -> bool {
+      *ch == b'\r' || *ch == b'\xFF'
+    });
+
+    match maybe_idx {
+      Some(idx) => {
+        let token = if idx == 0 {
+          None
+        } else {
+          Some(TelnetToken::Text(self.data[0..idx]))
+        };
+
+        let state = if self.data[idx] == b'\r' {
+          Carriage
+        } else {
+          Command
+        };
+
+        (token, state, self.data[idx+1..])
+      }
+      None => {
+        // This whole data block is text.
+        (Some(TelnetToken::Text(self.data)), Neutral, b"")
+      }
+    }
+  }
+
+  pub fn step_parser(&mut self) -> Option<TelnetToken<'b>> {
+    if self.data.is_empty() {
+      return None
+    }
+
+    let (token, state, data) = match self.state.state {
+      Neutral => {
+        self.neutral_state()
+      }
+      Carriage => {
+        self.carriage_state()
+      }
+      Command => {
+        self.command_state()
+      }
+      Subchannel(command) => {
+        self.subchannel_state(command)
+      }
+    };
+
+    self.state.state = state;
+    self.data = data;
+    return token;
+  }
+}
+
+impl<'a, 'b> Iterator<TelnetToken<'b>> for TokenStream<'a, 'b> {
+  fn next(&mut self) -> Option<TelnetToken<'b>> {
+    while !self.data.is_empty() {
+      let token = self.step_parser();
       if token.is_some() {
         return token;
       }
     }
 
     return None;
-  }
-}
-
-
-
-pub type ParseResult<'a> = (Option<TelnetToken<'a>>, ParseState, &'a [u8]);
-
-fn is_negotiation(cmd: u8) -> bool {
-  250 <= cmd && cmd <= 254
-}
-
-
-fn carriage_state<'a>(data: &'a [u8]) -> ParseResult {
-  if data[0] == b'\n' {
-    (Some(TelnetToken::Text(b"\r\n")), Neutral, data[1..])
-  } else if data[0] == b'\0' {
-    (Some(TelnetToken::Text(b"\r")), Neutral, data[1..])
-  } else {
-    // invalid stream, technically, but still unambiguous
-    (Some(TelnetToken::Text(b"\r")), Neutral, data)
-  }
-}
-
-fn command_state<'a>(data: &'a [u8]) -> ParseResult {
-  if is_negotiation(data[0]) {
-    (None, Subchannel(data[0]), data[1..])
-  } else if data[0] == b'\xFF' {
-    (Some(TelnetToken::Text(b"\xFF")), Neutral, data[1..])
-  } else {
-    (Some(TelnetToken::Command(data[0])), Neutral, data[1..])
-  }
-}
-
-fn subchannel_state<'a>(data: &'a [u8], command: u8) -> ParseResult {
-  (Some(TelnetToken::Negotiation{command: command, channel: data[0]}), Neutral, data[1..])
-}
-
-fn neutral_state<'a>(data: &'a [u8]) -> ParseResult {
-  let maybe_idx = data.iter().position(|ch: &u8| -> bool {
-    *ch == b'\r' || *ch == b'\xFF'
-  });
-
-  match maybe_idx {
-    Some(idx) => {
-      let token = if idx == 0 {
-        None
-      } else {
-        Some(TelnetToken::Text(data[0..idx]))
-      };
-
-      let state = if data[idx] == b'\r' {
-        Carriage
-      } else {
-        Command
-      };
-
-      (token, state, data[idx+1..])
-    }
-    None => {
-      // This whole data block is text.
-      (Some(TelnetToken::Text(data)), Neutral, b"")
-    }
-  }
-}
-
-pub fn step_parser<'a>(state: ParseState, data: &'a [u8]) -> ParseResult {
-  if data.is_empty() {
-    return (None, state, data)
-  }
-
-  match state {
-    Neutral => {
-      neutral_state(data)
-    }
-    Carriage => {
-      carriage_state(data)
-    }
-    Command => {
-      command_state(data)
-    }
-    Subchannel(command) => {
-      subchannel_state(data, command)
-    }
   }
 }
